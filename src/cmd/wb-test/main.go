b466507f26c6d58835c2ec57192637db21650337
sanyokbig <sanyokbig@gmail.com>
2017-09-26 15:39:59 +0300

    core done

--- src/cmd/wb-test/main.go
+++ src/cmd/wb-test/main.go
@@ -4,34 +4,75 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"net/http"
+	"io/ioutil"
+	"strings"
 )
 
 func init() {
 	log.SetFlags(0)
 }
 
-// Other funcs, if any.
-
 func main() {
-
-	// Initialization, if any.
-
+	k := 2;
+	processingChannel := make(chan string, k); // Channel for URLs
+	doneChan := make(chan bool);               // Used when all urls handled
+	results := make(map[string]int);
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
+		url := scanner.Text();
 
-		// Do something with strings here.
+		// Run handler
+		processingChannel <- url
 
+		go handleUrl(url, results, processingChannel, doneChan)
 	}
+
 	if err := scanner.Err(); err != nil {
 		log.Fatalln(err)
 	}
 
-	// Other code, if any.
+	// waiting for all handlers
+	<-doneChan;
 
 	total := 0
 
-	// Other code, if any.
+	// Printing results for each url
+	for key, value := range results {
+		log.Println(key, ':', value)
+		total += value;
+	}
 
 	log.Printf("Total: %v", total)
+}
+
+func handleUrl(url string, results map[string]int, processingChannel chan string, doneChan chan bool) {
+	// Go and get count
+	res := countGoEntries(request(url));
+
+	// Store result
+	results[url] = res;
+
+	// Pull processed url so next can be pushed
+	<-processingChannel;
+	// if no url pushed we are done, call fot it
+	if len(processingChannel) == 0 {
+		doneChan <- true;
+	};
+}
+
+func request(url string) string {
+	res, err := http.Get(url)
+
+	if err != nil {
+		log.Fatalln(err);
+	}
+
+	bodyBytes, _ := ioutil.ReadAll(res.Body);
+	return string(bodyBytes);
+}
+
+func countGoEntries(s string) int {
+	return strings.Count(s, "Go")
 }
